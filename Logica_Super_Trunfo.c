84098eefa6a64f4e60888c09c399ac24e9ddca85
ProgramacaoP <paulalisboarp@gmail.com>
2025-04-17 00:02:34 +0000

    DEFINNIÇÃO E INICIALIZAÇÃO DE VARIAVEIS

--- Logica_Super_Trunfo.c
+++ Logica_Super_Trunfo.c
@@ -4,5 +4,21 @@ int main() {
     printf("Cartas I e II do Jogo Super Trunfo\n");
     // Acima printf para informando quis cartas estão sendo cadastradas - Nivel Novato tema 2
 
+    
+    // Abaixo definição de variáveis para cada atributo da carta - Nivel Novato tema 2
+    // Definição da estrutura para as cartas
+    typedef struct {
+        char estado[3];
+        char codigo[5];
+        char cidade[12];
+        int populacao;
+        float area;
+        float pib;
+        int pontosTuristicos;
+        float densidadePopulacional;
+        float pibPerCapita;
+        float SuperPoder;
+    } 
+
     return 0;
 }

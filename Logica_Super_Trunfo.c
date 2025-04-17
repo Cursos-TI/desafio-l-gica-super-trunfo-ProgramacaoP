c48928f1edb7da51e46cba97bdf4fb17142b26ec
ProgramacaoP <paulalisboarp@gmail.com>
2025-04-17 00:00:28 +0000

    INICIANDO CODIGO COM INCLIDE, INT MAIN, PRINTF, CHAVES E COMENTARIO

---
+++ Logica_Super_Trunfo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Cartas I e II do Jogo Super Trunfo\n");
+    // Acima printf para informando quis cartas estão sendo cadastradas - Nivel Novato tema 2
+
+    return 0;
+}
